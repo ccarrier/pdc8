dfaddc4ce5d9f4f725c2f5e2b4fac52c3761aa28
Camille Carrier <camille.carrier@insa-lyon.fr>
2014-12-16 17:40:35 +0100

    first commit

---
+++ src/fr/apln/pdc8/MainActivity.java
@@ -0,0 +1,16 @@
+package fr.apln.pdc8;
+
+
+import fr.apln.pdc8.R;
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class MainActivity extends Activity{
+
+	@Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+	}
+}
